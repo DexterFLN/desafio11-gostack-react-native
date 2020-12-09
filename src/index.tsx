4146b629b98db7281294cfe1ba963c8993a49bb0
DexterFLN <dragon_dexter-x@yahoo.com.br>
2020-12-09 20:32:02 -0300

    Uploading the challenge on the new folder

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,15 @@ import 'intl/locale-data/jsonp/pt-BR';
 
 import Routes from './routes';
 
-const App: React.FC = () => {
-  return (
-    <View style={{ flex: 1 }}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Routes />
-    </View>
-  );
-};
+const App: React.FC = () => (
+  <View style={{ flex: 1 }}>
+    <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+    />
+    <Routes />
+  </View>
+);
 
 export default App;
